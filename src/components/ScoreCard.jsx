7370cb80e5076ca2f982761937f2cc4b7d11f822
phuopham <108047552+phuopham@users.noreply.github.com>
2023-01-16 20:47:38 +0700

    add match type and time stamp

--- src/components/ScoreCard.jsx
+++ src/components/ScoreCard.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
-const ScoreCard = ({ team1, team2, score1, score2 }) => {
+const ScoreCard = ({ team1, team2, score1, score2, type, time }) => {
     return (
-        <div className='bg-gray-800 p-5 rounded-3xl w-full min-w-[200px]'>
+        <div className='bg-gray-800 p-5 rounded-3xl w-full min-w-[200px] relative mt-5'>
             <div className='flex justify-between'>
                 <div className={
-                    score1 > score2 ? 'text-primary' : 
-                    score1 == score2? '':'text-gray-500'
+                    score1 > score2 ? 'text-primary' :
+                        score1 == score2 ? '' : 'text-gray-500'
                 }>
                     {team1}
                 </div>
@@ -14,9 +14,12 @@ const ScoreCard = ({ team1, team2, score1, score2 }) => {
                     {score1} - {score2}
                 </div>
                 <div className={
-                    score2 > score1 ? 'text-primary' : 
-                    score1 == score2? '':'text-gray-500'
+                    score2 > score1 ? 'text-primary' :
+                        score1 == score2 ? '' : 'text-gray-500'
                 }>{team2}</div>
+            </div>
+            <div className="absolute top-[-0.75rem] right-1/2 translate-x-1/2 justify-between text-gray-200 bg-gray-700 px-2 py-1 rounded-xl">
+                <div className='text-xs'><span className='text-primary'>{type}</span> - {time}</div>
             </div>
         </div>
     )
